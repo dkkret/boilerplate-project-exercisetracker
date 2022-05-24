78f7e9f8b697ee80c9162d657398f05c0038063a
dkkret <dk.kret@gmail.com>
2022-05-24 21:32:04 +0200

    versionKey false

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const {Schema} = mongoose
 
 const userSchema = new Schema({
   name: {type: String, unique: true}
-})
+},
+{versionKey: false})
 const User = mongoose.model('User', userSchema)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
